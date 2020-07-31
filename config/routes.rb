53714a1454768e753b8b1e747f98bdb2ad89c384
K80Git <67084301+K80Git@users.noreply.github.com>
2020-07-31 14:12:07 +0900

    delete buys.scss etc

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,4 @@ Rails.application.routes.draw do
   resources :users, only: :show
   get 'after_logout', to: 'users#logout'
   delete 'user_delete', to: 'users#delete'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
