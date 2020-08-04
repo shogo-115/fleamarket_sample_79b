c59ecb33f932dacd4aa89472d4a4c1b117664b44
K80Git <67084301+K80Git@users.noreply.github.com>
2020-08-04 19:29:46 +0900

    create_render

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :product, dependent: :destroy
   belongs_to :user, optional: true
 
-  validates :text ,presence: true ,lenght:{maxmum: 300}
+  validates :text, presence: true, lenght:{maximum: 300}
 end
