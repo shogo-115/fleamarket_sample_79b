203a2a73a8a10b02f8b466f70da9625bbcfbac30
shogo-115 <izumi.may.11@gmail.com>
2020-08-05 20:55:33 +0900

    product-buy#1

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,11 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end
   get 'newlook', to: 'newlooks#index'
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_050232) do
+ActiveRecord::Schema.define(version: 2020_08_05_112937) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postNo", null: false
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2020_08_03_050232) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "customer_id"
+    t.string "card_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
