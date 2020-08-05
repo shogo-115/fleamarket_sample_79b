ab3d2e2d17173ef2a9fe29cc64602b49c01965b7
shogo-115 <izumi.may.11@gmail.com>
2020-08-05 19:09:27 +0900

    Revert "category-edit#1"
    This reverts commit 857e1da35275cd4cba48bbdfccc8e6d3ce3d6643.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,6 @@ class User < ApplicationRecord
          validates :nick_name ,presence: true
   has_one :address, dependent: :destroy
   has_one :name, dependent: :destroy
-<<<<<<< HEAD
   has_many :comments
   has_many :products
-=======
->>>>>>> parent of cda564d... 商品出品機能
 end
