dc23e099b9f7585cb9fb0e4691c522e5620375aa
shogo-115 <izumi.may.11@gmail.com>
2020-08-07 14:11:35 +0900

    product-all-fix#2

--- app/controllers/mens_controller.rb
+++ app/controllers/mens_controller.rb
@@ -3,7 +3,7 @@ class MensController < ApplicationController
   def index
     @products = Product.where(category_id: 2)
     @products = Product.includes(:images).where(category_id: 2)
-    @products = Product.all.page(params[:page]).per(6).where(category_id: 2).order('created_at DESC')
+    @products = Product.all.page(params[:page]).per(18).where(category_id: 2).order('created_at DESC')
   end
 
 end
--- app/controllers/womens_controller.rb
+++ app/controllers/womens_controller.rb
@@ -3,7 +3,7 @@ class WomensController < ApplicationController
   def index
     @products = Product.where(category_id: 1)
     @products = Product.includes(:images).where(category_id: 1)
-    @products = Product.all.page(params[:page]).per(6).where(category_id: 1).order('created_at DESC')
+    @products = Product.all.page(params[:page]).per(18).where(category_id: 1).order('created_at DESC')
   end
 
 end
