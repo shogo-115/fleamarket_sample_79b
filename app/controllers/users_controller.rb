f3b278c37714e366ce5e5fce8f618c045fabcb34
takuya617 <nishihara0617@gmail.comcdgit config --global user.email nishihara0617@gmail.com>
2020-08-10 17:33:58 +0900

    MypageEdit2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,17 @@ class UsersController < ApplicationController
 
   def show
     user = User.find(params[:id])
-    @nick_name = user.nick_name
-    @products = Product.where(user_id: params[:id]).last
-    @proImgs = @products.images.first
+    if current_user.id == user.id
+      @nick_name = user.nick_name
+      @products = Product.where(user_id: params[:id])
+      @products = Product.includes(:images).where(user_id: params[:id])
+      @products = Product.all.page(params[:page]).per(18).where(user_id: params[:id]).order('created_at DESC')
+      @productsB = Product.where(buyerId: params[:id])
+      @productsB = Product.includes(:images).where(buyerId: params[:id])
+      @productsB = Product.all.page(params[:page]).per(18).where(buyerId: params[:id]).order('created_at DESC')
+    else
+      redirect_to root_path
+    end
   end
   
   def logout
