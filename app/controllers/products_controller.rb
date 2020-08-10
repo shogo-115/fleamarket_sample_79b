935ad0f5c65d637318bf9461c02bf701d16581f6
takuya617 <nishihara0617@gmail.comcdgit config --global user.email nishihara0617@gmail.com>
2020-08-10 12:46:17 +0900

    MypageEdit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,8 +31,7 @@ class ProductsController < ApplicationController
     end
   end
 
-  def show
-  end
+  
 
   def edit
   end
@@ -54,6 +53,7 @@ class ProductsController < ApplicationController
     @producter = @product.user
     @categorie = @product.category
     @proImgs = @product.images
+    @proImge = @proImgs.first
     @comment = Comment.new
     @comments = @product.comments
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    @nick_name = current_user.nick_name
+    user = User.find(params[:id])
+    @nick_name = user.nick_name
+    @products = Product.where(user_id: params[:id]).last
+    @proImgs = @products.images.first
   end
   
   def logout
